6ca5c2d9725662b9e39d3c4bb07ae832488803dc
Roman Koff <krv@guap.ru>
2026-01-22 23:32:41 +0300

    2026-001-22

---
+++ ~classes/PaginatedQueryableHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace Ans.Net10.Common
+{
+
+	public class PaginatedQueryableHelper<TEntity>
+		where TEntity : class
+	{
+
+		/* ctor */
+
+
+		public PaginatedQueryableHelper(
+			IQueryable<TEntity> query,
+			int page,
+			int itemsOnPage)
+		{
+			var totalItems1 = query.Count();
+			Query = query
+				.Skip((page - 1) * itemsOnPage)
+				.Take(itemsOnPage);
+			PaginationHelper = new(itemsOnPage, totalItems1, page);
+		}
+
+
+		/* readonly properties */
+
+
+		public PaginationHelper PaginationHelper { get; }
+		public IQueryable<TEntity> Query { get; }
+
+	}
+
+}
